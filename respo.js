2a753f204d31fa7c2a741b347d44fcd2e07f6ff7
iki1uc <entertrainer@web.de>
2026-06-20 23:05:13 +0200

    Add command logging functionality to respo.js

--- respo.js
+++ respo.js
@@ -1,1 +1,14 @@
+const log = document.getElementById("log");
+
+document.querySelectorAll(".cmd").forEach(btn => {
+  btn.addEventListener("click", () => {
+    const cmd = btn.dataset.cmd;
+    respoCommand(cmd);
+  });
+});
+
+function respoCommand(cmd) {
+  log.innerHTML += `<div>${cmd}</div>`;
+  log.scrollTop = log.scrollHeight;
+}
 
